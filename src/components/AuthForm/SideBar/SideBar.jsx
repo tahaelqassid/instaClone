e57c2c450534ab5a80cc4f76e8ebd4ab3a621bec
TahaElQassiD <103741160+tahaelqassid@users.noreply.github.com>
2024-08-03 02:15:40 +0100

    files Updated

--- src/components/AuthForm/SideBar/SideBar.jsx
+++ src/components/AuthForm/SideBar/SideBar.jsx
@@ -1,9 +1,53 @@
+import { Box, Flex, Link } from "@chakra-ui/react"
+import {Link as RouterLink} from "react-router-dom"
+import { InstagramLogo, InstagramMobileLogo } from "../../../assetss/constants"
 
 
 const SideBar = () => {
-  return (
-    <div>SideBar</div>
-  )
+    return <Box 
+    height={"100vh"}
+    borderRight={"1px solid"}
+    borderColor={"whiteAlpha.300"}
+    py={0}
+    position={"sticky"}
+    top={0}
+    left={0}
+    px={{base:2,md:4}}
+    >
+
+        <Flex direction={"column"} gap={10} w={"full"} height={"full"}>
+            <Link to ={'/'} as ={RouterLink} p={2} display={{base:"none",md:"block"}} cursor={"pointer"}>
+                <InstagramLogo />
+            </Link>
+            <Link to ={'/'} as ={RouterLink} p={2} display={{base:"block",md:"none"}}
+            borderRadius={6}
+            _hover={{
+                bg:"whiteAlpha.200"
+            }}
+            w={10}
+            
+            cursor={"pointer"}>
+
+                <InstagramMobileLogo />
+            </Link>
+        </Flex>
+
+    
+
+        
+
+
+    </Box>
+
+
+  
+
+
+  
+  
+ 
+    
+  
 }
 
 export default SideBar
