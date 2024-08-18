929cd99ab67cefddfe71e0877acf0f3bfa84fbe3
TahaElQassiD <103741160+tahaelqassid@users.noreply.github.com>
2024-08-18 14:12:09 +0100

    new commits

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,8 +1,60 @@
+import { Flex, GridItem, Image, Text } from "@chakra-ui/react"
+import { AiFillHeart } from "react-icons/ai"
+import {FaComment} from "react-icons/fa"
 
-const Profilepost = () => {
+const ProfilePost = (img) => {
   return (
-    <div>Profilepost</div>
+    <GridItem
+    cursor={"pointer"}
+    borderRadius={4}
+    overflow={"hidden"}
+    border={"1px solid"}
+    borderColor={"WhiteAlpha.300"}
+    position={"relative"}
+    aspectRatio={1/1}
+    
+    
+    >
+        <Flex
+        opacity={0}
+        _hover={{opacity:1}}
+        position={"absolute"}
+        top={0}
+        left={0}
+        right={0}
+        bottom={0}
+        bg={"blackAlpha.700"}
+        transition={"all 0.3s ease"}
+        zIndex={1}
+        justifyContent={"center"}
+        
+        >
+            <Flex alignItems={"center"} justifyContent={"center"} gap={50}>
+                <Flex>
+                    <AiFillHeart size={20} />
+                    <Text fontWeight={"bold"}>
+                        77
+                    </Text>
+                </Flex>
+                <Flex>
+                    <FaComment size={20} />
+                    <Text fontWeight={"bold"} ml={2}>
+                        68
+
+                    </Text>
+                </Flex>
+
+            </Flex>
+
+
+        </Flex>
+
+        <Image src={img} alt="Profile post" w={"100%"} h={"100%"} objectFit={"cover"} />
+
+
+
+    </GridItem>
   )
 }
 
-export default Profilepost
+export default ProfilePost
--- src/components/Profile/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts.jsx
@@ -1,6 +1,58 @@
+import { Box, Grid, Skeleton, VStack } from "@chakra-ui/react"
+import { useState,useEffect } from "react"
+import ProfilePost from "./ProfilePost"
+
+
 const ProfilePosts = () => {
+    const [isLoading,setIsLoading] = useState(true)
+
+    useEffect (() => {
+        setTimeout(() => {
+            setIsLoading(false)
+
+        },2000)
+    },[])
+
+
   return (
-    <div>ProfilePosts</div>
+    <Grid 
+    templateColumns={{
+        sm:"repeat(1,1fr)",
+        md:"repeat(3,1fr)"
+    }}
+    gap={1}
+    columnGap={1}
+    
+    >
+        {isLoading && [0,1,2,3,4,5].map((_,idx) => (
+            <VStack key={idx} alignItems={"flex-start"}>
+                <Skeleton w={"full"}>
+                    <Box h="300px">contents wrapped</Box>
+
+                </Skeleton>
+            </VStack>
+
+        )
+        )}
+
+
+        {!isLoading && (
+            <>
+            <ProfilePost src="/img1.png" />
+            
+            
+            
+            
+            
+            </>
+
+
+
+        )}
+
+
+
+    </Grid>
   )
 }
 
